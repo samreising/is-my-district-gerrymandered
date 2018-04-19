ac6c1ee0939ddb79cec5ff8a11f5c7fb67846015
Sam Reising <samuel.p.reising@gmail.com>
2018-04-18 20:13:14 -0400

    Refactor code to be more OOP and add data encapsulation

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, jsonify, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.heroku import Heroku
+from flask_heroku import Heroku
 from flask_jsglue import JSGlue
 from apiclient.discovery import build
-from werkzeug.exceptions import HTTPException
-import json
 import os
 
 # add API keys
+from werkzeug.exceptions import HTTPException
+
 CIVIC_API_KEY = os.environ['CIVIC_API_KEY']
 MAPS_API_KEY = os.environ['MAPS_API_KEY']
 
 # configure application
 app = Flask(__name__)
 JSGlue(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/gerrymander'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
-from models import Calculations, Fips_Codes
-from helpers import lookup_district
+from helpers import District
 
 # ensure responses aren't cached
 if app.config['DEBUG']:
@@ -46,26 +44,29 @@ def district():
     # if address is NULL, redirect to index page
     if not address:
         return redirect('/')
-    
+
+    # create new District object
+    cd = District()
+
     # call Google Civic Information API
     service = build('civicinfo', 'v2', developerKey = CIVIC_API_KEY)
-    response = service.representatives().representativeInfoByAddress(levels='country', roles = None, address = address, includeOffices = False).execute()
+    cd.response = service.representatives().representativeInfoByAddress(levels='country', roles = None, address = address, includeOffices = False).execute()
 
     # if response is NULL, redirect to index page
-    if not response:
+    if not cd.response:
         return redirect('/')
 
     # lookup district
-    district = lookup_district(response)
+    cd.lookup_district()
     
     # render template with congressional district
-    return render_template('district.html', state = district['state'], full_district_name = district['full district name'], intpt_lat = district['intpt_lat'], intpt_lon = district['intpt_lon'], geoid = district['geoid'], gerrymandered_msg = district['gerrymandered'][0], gerrymandered_class = district['gerrymandered'][1], MAPS_API_KEY = MAPS_API_KEY)
+    return render_template('district.html', state = cd.state, congressional_district = cd.congressional_district, geoid = cd.geoid, intpt_lat = cd.intpt_lat, intpt_lon = cd.intpt_lon, message = cd.message, message_class = cd.message_class, MAPS_API_KEY = MAPS_API_KEY)
     
 @app.errorhandler(Exception)
 def handle_error(e):
     # redirect all errors to the home page
     if isinstance(e, HTTPException):
-        code = e.code
+        e.code
     return render_template('index.html', error = "true")
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -1,84 +1,107 @@
 from models import Calculations, Fips_Codes
 
-def lookup_district(response):
-# create list to store district information in
-    district = []
-    geoid = None
-    state = None
+class District:
 
-    # get the values from name keys under divisions in JSON
-    for division in response['divisions'].items():
-        for name in division[1].items():
-            if len(name[1][0]) == 1:
-                district.append(name[1])
-            else:
-                district.append(name[1][0])
-    
-    # remove items containing 'ocd-division/country:us/state:' from list
-    district = [i for i in district if 'ocd-division/country:us/state:' not in i]
-    
+    def __init__(self):
+        self.__district_list = []
+
+        self.response = None
+        self.state = None
+        self.congressional_district = None
+        self.geoid = None
+        self.intpt_lat = None
+        self.intpt_lon = None
+        self.message = None
+        self.message_class = None
+        self.__fips_code = None
+        self.__at_large = None
+        self.__polsby_popper = None
+
+    def lookup_district(self):
+        self.__set_name_key_values()
+        self.__remove_string_items('ocd-division/country:us/state:')
+        self.__clean_up()
+        self.__append_with_string('\'s at-large congressional district')
+        self.__set_state()
+        self.__set_congressional_district()
+        self.__set_fips_code()
+        self.__set_at_large()
+        self.__set_geoid()
+        self.__set_polsby_popper_score()
+        self.__set_intpt_lat()
+        self.__set_intpt_lon()
+        self.__set_message_and_message_class()
+
+    # get the values from name keys under divisions in JSON; district_list.[0] = state; district_list.[1] = congressional district
+    def __set_name_key_values(self):
+        self.__district_list = []
+        for division in self.response['divisions'].items():
+            for name in division[1].items():
+                if len(name[1][0]) == 1:
+                    self.__district_list.append(name[1])
+                else:
+                    self.__district_list.append(name[1][0])
+
+    # remove items containing a string from list, remove 'United States', sort
+    def __remove_string_items(self, string):
+        self.__district_list = [x for x in self.__district_list if string not in x]
+
     # remove "United States" from list and sort so [0] is state and [1] is full congressional district
-    district.remove('United States')
-    district.sort()
-    
-    # if no district, append with 'at-large congressional district'
-    if len(district) == 1:
-       district.append(district[0] + '\'s at-large congressional district')
+    def __clean_up(self):
+        self.__district_list.remove('United States')
+        self.__district_list.sort()
 
-    # get fips code
-    fips_code = Fips_Codes.query.filter_by(state=district[0]).first().fips_code
-    district.append(fips_code)
-    
-    # get whether or not state is an at-large district
-    at_large = Fips_Codes.query.filter_by(state=district[0]).first().at_large
-    
-    # determine GEOID
-    if at_large == False:
-        district_number = int(''.join(list(filter(str.isdigit, district[1]))))
-        if district_number < 10:
-            district_number = '0' + str(district_number)
+    # if no district, append with 'at-large congressional district'
+    def __append_with_string(self, string):
+        if len(self.__district_list) == 1:
+            self.__district_list.append(self.__district_list[0] + string)
+
+    # set state
+    def __set_state(self):
+        self.state = self.__district_list[0]
+
+    # set congressional district
+    def __set_congressional_district(self):
+        self.congressional_district = self.__district_list[1]
+
+    # set fips code
+    def __set_fips_code(self):
+        self.__fips_code = Fips_Codes.query.filter_by(state=self.state).first().fips_code
+        self.__district_list.append(self.__fips_code)
+
+    # set whether or not state is an at-large district
+    def __set_at_large(self):
+        self.__at_large = Fips_Codes.query.filter_by(state=self.state).first().at_large
+
+    # set GEOID
+    def __set_geoid(self):
+        if self.__at_large == False:
+            district_number = int(''.join(list(filter(str.isdigit, self.congressional_district))))
+            if district_number < 10:
+                district_number = '0' + str(district_number)
+            else:
+                district_number = str(district_number)
         else:
-            district_number = str(district_number)
-    else:
-        district_number = '00'
-    geoid = fips_code + district_number
-    district.append(geoid)
-    
-    # get Polsby-Popper Score
-    polsby_popper = Calculations.query.filter_by(geoid=district[3]).first().polsby_popper_score
-    if polsby_popper:
-        district.append(polsby_popper)
-    else:
-        district.append('0')
-    
-    # get intpt_lat
-    intpt_lat = Calculations.query.filter_by(geoid=district[3]).first().intpt_lat
-    if intpt_lat:
-        district.append(intpt_lat)
-    else:
-        district.append('0')
-    
-    # get intpt_lon
-    intpt_lon = Calculations.query.filter_by(geoid=district[3]).first().intpt_lon
-    if intpt_lon:
-        district.append(intpt_lon)
-    else:
-        district.append('0')
-        
+            district_number = '00'
+        self.geoid = self.__fips_code + district_number
+
+    # set Polsby-Popper Score
+    def __set_polsby_popper_score(self):
+        self.__polsby_popper = Calculations.query.filter_by(geoid=self.geoid).first().polsby_popper_score
+
+    # set intpt_lat
+    def __set_intpt_lat(self):
+        self.intpt_lat = Calculations.query.filter_by(geoid=self.geoid).first().intpt_lat
+
+    # set intpt_lon
+    def __set_intpt_lon(self):
+        self.intpt_lon = Calculations.query.filter_by(geoid=self.geoid).first().intpt_lon
+
     # if Polsby-Popper score is less than 14, the district is gerrymandered
-    if polsby_popper < 14:
-        district.append(['Your district is likely gerrymandered!', 'text-danger'])
-    else:
-        district.append(['Your district is likely not gerrymandered!', 'text-success'])
-    
-    # district should contain state, full district name, district (number), GeoID, Polsby-Popper Score, INTLAT, INTLONG, gerrymandered message/class
-    return {
-        'state': district[0],
-        'full district name': district[1],
-        'district': district[2],
-        'geoid': district[3],
-        'polsy_popper': district[4],
-        'intpt_lat': district[5],
-        'intpt_lon': district[6],
-        'gerrymandered': district[7]
-    }
+    def __set_message_and_message_class(self):
+        if self.__polsby_popper < 14:
+            self.message = 'Your district is likely gerrymandered!'
+            self.message_class = 'text-danger'
+        else:
+            self.message = 'Your district is likely not gerrymandered!'
+            self.message_class = 'text-success'
